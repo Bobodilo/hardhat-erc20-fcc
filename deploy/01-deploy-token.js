ac1205aac5c56e74bee6e72ce3b113e0867d3d5a
bonheurbaleks <bonheurbalekoua@gmail.com>
2023-08-07 21:12:30 +0300

    first commit

--- deploy/01-deploy-token.js
+++ deploy/01-deploy-token.js
@@ -1,3 +1,4 @@
+//imports
 const { network } = require("hardhat")
 const {
   developmentChains,
@@ -5,23 +6,25 @@ const {
 } = require("../helper-hardhat-config")
 const { verify } = require("../helper-functions")
 
+//main
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
-  const ourToken = await deploy("OurToken", {
+  const OurToken = await deploy("OurToken", {
     from: deployer,
-    args: [INITIAL_SUPPLY],
     log: true,
-    // we need to wait if on a live network so we can verify properly
-    waitConfirmations: network.config.blockConfirmations || 1,
+    args: [INITIAL_SUPPLY],
+    //wait for confirmation
+    waitforconfirmations: network.config.blockConfirmations || 1,
   })
-  log(`ourToken deployed at ${ourToken.address}`)
+
+  log(`OurToken was deployed at ${OurToken.address}`)
 
   if (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
   ) {
-    await verify(ourToken.address, [INITIAL_SUPPLY])
+    await verify(OurToken.address, [INITIAL_SUPPLY])
   }
 }
 
